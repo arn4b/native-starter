6cb3d6388ae2b98b09322bf6200669bce7eaf7f6
Arnab Chatterjee <arnabc1701@gmail.com>
2021-05-30 12:39:03 +0530

    DONE

--- App.js
+++ App.js
@@ -1,42 +1,109 @@
 import React, { useState } from "react";
-import { Text, StyleSheet, View } from "react-native";
-
+import { Text, StyleSheet, View, KeyboardAvoidingView, Platform, TextInput, TouchableOpacity, Keyboard } from "react-native";
+import Task from './components/Task'
 const TextInANest = () => {
-  const [titleText, setTitleText] = useState("Bird's Nest");
-  const bodyText = useState("This is not really a bird nest.");
+  
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
 
-  const onPressTitle = () => {
-    setTitleText("Bird's Nest [pressed]");
-  };
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+    setTask(null)
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+    console.log("deleted");
+  }
 
   return (
-    <View style={styles.viewBox}>
-    <Text style={styles.baseText}>
-      <Text style={styles.titleText} onPress={onPressTitle}>
-        {titleText}
-        {"\n"}
-        {"\n"}
-      </Text>
-      <Text numberOfLines={5}>{bodyText}</Text>
-    </Text>
+    <View style={styles.container}>
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+
+        <View style={styles.items}>
+          {/* THis is where the tabs will go  */}
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                  <Task text={item} />
+                </TouchableOpacity>
+              );
+            })
+          }
+          
+        </View>
+      </View>
+
+      {/* Write a task */}
+
+      <KeyboardAvoidingView 
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.writeTaskWrapper} >
+        <TextInput style={styles.input} placeholder={"Write a task"} value={task} onChangeText={text => setTask(text)}/>
+          <TouchableOpacity onPress={() => handleAddTask()}>
+            <View style={styles.addWrapper}>
+              <Text style={styles.addText}>+</Text>
+            </View>
+          </TouchableOpacity>
+      </KeyboardAvoidingView>
+
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  baseText: {
-    
+  container: {
+    flex: 1,
+    backgroundColor: '#ebeaed',
   },
-  titleText: {
-    fontSize: 20,
-    fontWeight: "bold"
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
   },
-  viewBox: {
-    marginTop: 50,
-    height:500,
-    width:500,
-    backgroundColor: 'red'
-  }
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {
+    marginTop: 15,
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 20,
+    width: '100%',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    width: '70%',
+    paddingHorizontal: 25,
+    backgroundColor: '#fff',
+    borderRadius: 35,
+    borderColor: '#d400ffa0',
+    borderWidth: 1,
+    marginLeft: 20
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: '#d400ffa0',
+    borderWidth: 1,
+    marginRight: 20
+  },
+  addText: {
+    fontSize: 30,
+  },
 });
 
 export default TextInANest;
